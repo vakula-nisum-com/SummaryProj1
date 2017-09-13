54bbbe32bbe83a43ce0f2204220d8c38da83c5e2
vijay akula <vakula@nisum.com>
2017-09-13 14:11:11 +0530

    added package to the newly created class

--- src/com/vijay/summaryproj1/test/TestClass2.java
+++ src/com/vijay/summaryproj1/test/TestClass2.java
@@ -1,7 +1,7 @@
-class TestClass2
-{
-public static void main(String[] args)
-{
-System.out.println("this is the second class we have to write");
-}
+package com.vijay.summaryproj1.test;
+
+public class TestClass2 {
+	public static void main(String[] args) {
+		System.out.println("this is the second class we have to write");
+	}
 }
